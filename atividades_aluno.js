cdfbf98067aac65b70fd383c31f5aa3f95c73950
Lívia Ozório <livia.ozorio@aluno.senai.br>
2024-10-25 17:15:26 -0300

    Create atividades_aluno.js

---
+++ atividades_aluno.js
@@ -0,0 +1,4 @@
+document.getElementById('toggle-atividades').addEventListener('click', function() {
+    const materias = document.getElementById('materias');
+    materias.classList.toggle('escondido');
+});
