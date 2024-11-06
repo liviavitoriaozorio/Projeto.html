8598ab53a858c2f20da9569ff4fe58ba04c760fc
The0ssi <beatriz.r.ferreira@aluno.senai.br>
2024-11-06 17:14:22 -0300

    Update AgendaScript.js

--- AgendaScript.js
+++ AgendaScript.js
@@ -323,7 +323,7 @@ function updateEvents(date) {
   });
   if (events === "") {
     events = `<div class="no-event">
-            <h3>No Events</h3>
+            <h3>Sem Eventos</h3>
         </div>`;
   }
   eventsContainer.innerHTML = events;
