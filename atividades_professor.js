fb3ac51293d1a199fc0b42ff66de68e20f6695b4
Laurinharamos <laura.r.silva12@aluno.senai.br>
2024-11-13 08:46:50 -0300

    Update atividades_professor.js

--- atividades_professor.js
+++ atividades_professor.js
@@ -1,35 +1,57 @@
-/* Responsavel pelo CSS do "ativo" funcionar  */
-const allSideMenu = document.querySelectorAll('#sidenav .menu.top li a');
+// Efeito dos elementos ativos 
+const allSideMenu = document.querySelectorAll('#Sidenav .Menu_lateral li a');
 
-allSideMenu.forEach(item=> {
-	const li = item.parentElement;
+allSideMenu.forEach(item => {
+    const li = item.parentElement;
 
-	item.addEventListener('click', function () {
-		allSideMenu.forEach(i=> {
-			i.parentElement.classList.remove('ativo');
-		})
-		li.classList.add('ativo');
-	})
+    item.addEventListener('click', function () {
+        allSideMenu.forEach(i => {
+            i.parentElement.classList.remove('ativo');
+        });
+        li.classList.add('ativo');
+    });
 });
 
 
-/* Esconde o sidenav */
-const menuBar = document.querySelector('#conteudo nav .fa-solid.fa-bars');
-const sidenav = document.getElementById('sidenav');
+
+// Esconde o sidenav
+const menuBar = document.querySelector('header .fa-solid.fa-bars');
+const sidebar = document.getElementById('Sidenav');
 
 menuBar.addEventListener('click', function () {
-	sidenav.classList.toggle('escondido');
-})
+    // Alterna a classe 'escondido' do sidenav
+    sidebar.classList.toggle('escondido');
 
+    // Fecha o submenu se o sidenav estiver escondido
+    const submenus = document.querySelectorAll('#Sidenav .Submenu');
+    submenus.forEach(submenu => {
+        submenu.classList.remove('showCollapse');
+    });
+
+    // Também pode resetar a rotação do ícone do submenu
+    const arrows = document.querySelectorAll('.fa-chevron-down.rotate');
+    arrows.forEach(arrow => {
+        arrow.classList.remove('rotate');
+    });
+});
+
+
+
+// Menu das minhas atividades 
 const linkCollapse = document.getElementsByClassName('fa-solid fa-chevron-down');
-var i
 
 for (let i = 0; i < linkCollapse.length; i++) {
-    linkCollapse[i].parentElement.addEventListener('click', function() {
+    linkCollapse[i].parentElement.addEventListener('click', function () {
         const collapseMenu = this.nextElementSibling;
         collapseMenu.classList.toggle('showCollapse');
+
+        // Verifica se o sidenav está escondido e, se sim, exibe-o
+        if (sidebar.classList.contains('escondido')) {
+            sidebar.classList.remove('escondido');
+        }
 
         const rotate = this.querySelector('.fa-chevron-down');
         rotate.classList.toggle('rotate');
     });
 }
+
