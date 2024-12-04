3022bcccf8560f8c899a06a59c1c7e77ab331ff1
The0ssi <beatriz.r.ferreira@aluno.senai.br>
2024-12-04 00:11:53 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,8 +1,8 @@
 const usuarios = [
-    { email: "matematica@lagunaschool.com", senha: "profmat123", tipo: "professor", disciplina: "matematica" },
-    { email: "portugues@lagunaschool.com", senha: "profport123", tipo: "professor", disciplina: "portugues" },
-    { email: "aluno1@lagunaschool.com", senha: "aluno123", tipo: "aluno", turma: "turma_a"},
-    { email: "aluno2@lagunaschool.com", senha: "aluno123", tipo: "aluno", turma: "turma_b"}
+    { nome: "Maria",email: "matematica@lagunaschool.com", senha: "profmat123", tipo: "professor", disciplina: "matematica" },
+    { nome: "Ana", email: "portugues@lagunaschool.com", senha: "profport123", tipo: "professor", disciplina: "portugues" },
+    { nome: "Marcos", email: "aluno1@lagunaschool.com", senha: "aluno123", tipo: "aluno", turma: "turma_a"},
+    { nome: "Thiago", email: "aluno2@lagunaschool.com", senha: "aluno123", tipo: "aluno", turma: "turma_b"}
 ];
 
 document.getElementById("login-btn").addEventListener("click", function() {
@@ -14,12 +14,15 @@ document.getElementById("login-btn").addEventListener("click", function() {
 
     if (usuario) {
         if (usuario.tipo === "professor") {
-            // Salva as informações do professor no LocalStorage, incluindo a disciplina
-            localStorage.setItem('professor', JSON.stringify(usuario));
+            // Salva as informações do professor e a disciplina no LocalStorage
+            localStorage.setItem(`professor_${usuario.disciplina}`, JSON.stringify(usuario));
+            localStorage.setItem('disciplinaAtual', usuario.disciplina); // Salva a disciplina atual para uso global
             window.location.href = `inicio_professor.html`;
         } else if (usuario.tipo === "aluno") {
-            // Redireciona para a página do aluno
+            // Salva o aluno e a turma no LocalStorage
             localStorage.setItem("usuarioLogado", JSON.stringify(usuario));
+            // Também salva a turma no localStorage separadamente
+            localStorage.setItem("selectedTurma", usuario.turma); // Salva a turma do aluno
             window.location.href = "inicio_aluno.html";
         }
     } else {
